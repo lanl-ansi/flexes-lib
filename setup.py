8eb5bc23640f3a3186c1ea85db22168bda7f140f
James Arnold <arnold_j@lanl.gov>
2017-07-25 17:20:41 -0600

    Fix bug in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='aws_utils',
-      description='Utilities for iteracting with Amazon Web Services'
+      description='Utilities for iteracting with Amazon Web Services',
       author='James Arnold',
-      author_email='arnold_j@lanl.gov'
+      author_email='arnold_j@lanl.gov',
       version='0.1',
       packages=['aws_utils'],
       license='MIT')

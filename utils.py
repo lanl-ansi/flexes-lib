36b4000e666a1855d462bd5808befab6649a8471
James Arnold <arnold_j@lanl.gov>
2017-10-04 12:14:18 -0600

    Fix path resolution for config, return hex signature
    Config loader wasn't resolving '~/' correctly. Adding os.path.expanduser fixed
    the problem. Also, the signature can't be sent as raw bytes. Instead the hex
    representation is sent.

--- utils.py
+++ utils.py
@@ -3,13 +3,16 @@ import hmac
 import os
 from configparser import ConfigParser
 
+def has_credentials(credentials):
+    return not None in credentials
+
 def get_credentials():
     api_key = os.environ.get('LANLYTICS_API_KEY')
     api_secret_key = os.environ.get('LANLYTICS_API_SECRET_KEY')
 
     credentials = (api_key, api_secret_key)
 
-    if not None in credentials:
+    if has_credentials(credentials):
         return credentials
     else:
         try:
@@ -19,8 +22,11 @@ def get_credentials():
 
 
 def load_config(config_file='~/.lanlytics/credentials'):
-    config = ConfigParser()
+    if '~' in config_file:
+        config_file = os.path.expanduser(config_file)
+
     if os.path.isfile(config_file):
+        config = ConfigParser()
         config.read(config_file)
     else:
         raise FileNotFoundError('{} not found'.format(config_file))
@@ -31,19 +37,19 @@ def load_config(config_file='~/.lanlytics/credentials'):
 
     credentials = (api_key, api_secret_key)
 
-    if not None in credentials:
+    if has_credentials(credentials):
         return credentials
 
 
 def sign_request_payload(payload):
     credentials = get_credentials()
 
-    if credentials is None:
+    if not has_credentials(credentials):
         raise ValueError('Invalid credentials')
 
     api_key = credentials[0]
     api_secret_key = credentials[1]
 
-    hashed_payload = hmac.new(api_secret_key.encode(), payload.encode(), hashlib.sha256).digest()
+    hashed_payload = hmac.new(api_secret_key.encode(), payload.encode(), hashlib.sha256).hexdigest()
 
     return api_key, hashed_payload
